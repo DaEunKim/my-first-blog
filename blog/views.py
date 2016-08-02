99c625eb0a6b41cdbe8a11fe061b1a077514e732
DaEunKim <ekdms717@naver.com>
2016-08-02 12:18:27 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 
 def post_list(request):
 	return render(request, 'blog/post_list.html', {})
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url="http://movie.naver.com/movie/sdb/rank/rmovie.nhn"
+soup = BeautifulSoup(urlopen(url).read())
+pkg_list=soup.findAll("div", "tit3")
+
+count = 1
+for i in pkg_list:
+	title = i.findAll('a')
+	print (count, "위: ", str(title)[str(title).find('title="')+7:str(title).find('">')])
+	count=count+1
+
